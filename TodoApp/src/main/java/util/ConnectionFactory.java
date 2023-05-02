76b6e3f6b3edc87558f14919b97eb63a0e8e483d
Virgílio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-01 21:42:46 -0300

    "add ConnectionFactory.java"

---
+++ TodoApp/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author virgi
+ */
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
+    public static final String USER = "root";
+    public static final String PASS = "";
+    
+}
