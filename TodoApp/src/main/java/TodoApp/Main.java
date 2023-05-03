d0c96ce3abeb73025f556fbcefb78caef64e6538
Virgí­lio Pires da Costa <virgilio.pires.costa@gmail.com>
2023-05-03 20:38:04 -0300

    Update Main.java test
    Update Main.java test

--- TodoApp/src/main/java/TodoApp/Main.java
+++ TodoApp/src/main/java/TodoApp/Main.java
@@ -18,18 +18,30 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
+//        ProjectController projectController = new ProjectController();
+//        
+//        Project project = new Project();
+//        project.setName("Projeto teste");
+//        project.setDescription("description");
+//        projectController.save(project);      
+
         ProjectController projectController = new ProjectController();
         
         Project project = new Project();
-        project.setName("Projeto teste");
+        project.setName("Projeto teste1");
         project.setDescription("description");
+        project.setId(12);
+        project.setName("Novo Nome do projeto");
+//        project.setDescription("description");    
+                
         projectController.save(project);
+        project.setId(12);
+        projectController.getUpdate(project);
         
-//        project.setName("Novo nome do projeto");
-//        projectController.update(project);
-//        
-//        List<Project>projects = projectController.getAll();
-//        System.out.println("Total de projetos" + projects.size());
+        List<Project>projects = projectController.getAll();
+        System.out.println("Total de projetos" + projects.size());
+        
+        //projectController.removeById(12;
         
     }
     
